13a7e40927f3a45e8b1451eea5c519e226af37f6
JayDalton <github@t-jay.net>
2022-11-24 14:26:08 +0100

    add http router

--- main.go
+++ main.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/julienschmidt/httprouter"
+	"github.com/rs/zerolog/log"
+)
 
 func main() {
 	fmt.Println("Hallo Welt!")
+	router := httprouter.New()
+
+	router.GET("/")
+
+	server := http.Server{Addr: ":3000", Handler: router}
+	err := server.ListenAndServe()
+
+	log.Fatal().Err(err).Msg("server failed")
 }
