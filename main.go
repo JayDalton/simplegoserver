98711e9d19f1ffc57ddc44b4ee864711c9b7a4b3
JayDalton <github@t-jay.net>
2022-11-24 14:46:09 +0100

    add first route

--- main.go
+++ main.go
@@ -12,10 +12,15 @@ func main() {
 	fmt.Println("Hallo Welt!")
 	router := httprouter.New()
 
-	router.GET("/")
+	router.GET("/", getRoot)
 
 	server := http.Server{Addr: ":3000", Handler: router}
 	err := server.ListenAndServe()
 
 	log.Fatal().Err(err).Msg("server failed")
+}
+
+func getRoot(writer http.ResponseWriter, request *http.Request, _ httprouter.Params) {
+	writer.WriteHeader(http.StatusOK)
+	writer.Write([]byte("Hallo, Welllt!!"))
 }
