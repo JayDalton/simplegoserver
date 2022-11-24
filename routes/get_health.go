2f16b4ecaea97b3f60c2f8ef6de8c699e84503dd
JayDalton <github@t-jay.net>
2022-11-24 17:41:59 +0100

    add json response

--- routes/get_health.go
+++ routes/get_health.go
@@ -1,15 +1,33 @@
 package routes
 
 import (
+	"encoding/json"
 	"net/http"
+	"time"
 
 	"github.com/JayDalton/simplegoserver/metrics"
 	"github.com/julienschmidt/httprouter"
 )
 
+type HealthResponse struct {
+	Uptime time.Duration `json:"uptime"`
+}
+
 func GetHealth() HttpLambda {
 	return func(writer http.ResponseWriter, request *http.Request, _ httprouter.Params) {
+		response := HealthResponse{
+			Uptime: metrics.Uptime(),
+		}
+
+		jsonResponse, err := json.Marshal(response)
+		if err != nil {
+			writer.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+
+		writer.Header().Set("Content-Type", "application/json")
 		writer.WriteHeader(http.StatusOK)
-		writer.Write([]byte(metrics.Uptime().String()))
+		writer.Write(jsonResponse)
+		// writer.Write([]byte(metrics.Uptime().String()))
 	}
 }
